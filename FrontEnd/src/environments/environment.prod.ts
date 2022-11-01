774b4fd3a451817d75d3fef89707a10172b75b88
patricia pucheta <ptrcpucheta@gmail.com>
2022-11-01 00:34:32 -0300

    avances

---
+++ FrontEnd/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontendpucheta',
+    appId: '1:1034210978448:web:17450bd5584b046ba8c615',
+    storageBucket: 'frontendpucheta.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyC_3PHQ2ay4kdd6_TJ5NSuwIYur_YyKQCc',
+    authDomain: 'frontendpucheta.firebaseapp.com',
+    messagingSenderId: '1034210978448',
+  },
+  production: true,
+  URL:'https://backendpucheta.herokuapp.com/'
+};
---
+++ FrontEnd/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'frontendpucheta',
+    appId: '1:1034210978448:web:17450bd5584b046ba8c615',
+    storageBucket: 'frontendpucheta.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyC_3PHQ2ay4kdd6_TJ5NSuwIYur_YyKQCc',
+    authDomain: 'frontendpucheta.firebaseapp.com',
+    messagingSenderId: '1034210978448',
+  },
+  production: false,
+  URL: 'http://localhost:8080/'
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
